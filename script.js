3c656d062e088f81f28caae92e6f241ad4fb8f2e
johnjohnn <conde2010@aol.com>
2021-07-21 20:28:36 -0400

    adding some javascript

---
+++ script.js
@@ -0,0 +1,12 @@
+const imgs = document.getElementById("imgs")
+const leftBtn = document.getElementById("left")
+const rightBtn = document.getElementById("right")
+
+
+const img =  document.querySelectorAll("#imgs img")
+
+
+
+let idx = 0 
+
+let interval =  setInterval(run)
